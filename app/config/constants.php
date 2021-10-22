4259b6e5b732b132420508382d84a07eccc4ccbd
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2021-10-22 14:14:09 +0700

    Ganti versi ke 1.5.2

--- app/config/constants.php
+++ app/config/constants.php
@@ -84,4 +84,4 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-defined('SIMASJID_VERSION')    OR define('SIMASJID_VERSION', '1.5.0');
+defined('SIMASJID_VERSION')    OR define('SIMASJID_VERSION', '1.5.2');

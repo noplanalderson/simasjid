c1864629c2f316a598c3f77bde55b2cd4c95f495
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2023-02-14 10:18:53 +0700

    Update codeigniter session library untuk kompatibilitas php 8

--- system/libraries/Session/SessionUpdateTimestampHandlerInterface.php
+++ system/libraries/Session/SessionUpdateTimestampHandlerInterface.php
@@ -39,9 +39,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * SessionHandlerInterface
+ * SessionUpdateTimestampHandlerInterface
  *
- * PHP 5.4 compatibility interface
+ * PHP 7 compatibility interface
  *
  * @package	CodeIgniter
  * @subpackage	Libraries
@@ -49,12 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author	Andrey Andreev
  * @link	https://codeigniter.com/userguide3/libraries/sessions.html
  */
-interface SessionHandlerInterface {
+interface SessionUpdateTimestampHandlerInterface {
 
-	public function open($save_path, $name);
-	public function close();
-	public function read($session_id);
-	public function write($session_id, $session_data);
-	public function destroy($session_id);
-	public function gc($maxlifetime);
+	public function updateTimestamp($session_id, $data);
+	public function validateId($session_id);
 }
